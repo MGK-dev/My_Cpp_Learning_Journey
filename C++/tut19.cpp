6cbf48ba500f9e003b406c23642a78a15339223c
unknown <khannsaab01@gmail.com>
2025-01-16 20:56:50 +0500

    In tut18.cpp learn and practice recursions, recursive functions for factorial numbers and fibonacci series. In tut19.cpp learn and practice function overloading and how it works by compiler.

---
+++ C++/tut19.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+
+// int add(int a, int b) {
+//     return a + b;
+// }
+
+// int add(int a, int b, int c) {
+//     return a + b + c;
+// }
+// calcualte the volume of a cylinder
+int volume (double r, int h) {
+    return (3.14 * r * r * h);
+}
+// calculate the volume of a cube
+int volume (int a) {
+    return (a * a *a);
+}
+//calculate the volume of a rectangle box
+int volume (int l, int b, int h) {
+    return (l * b * h);
+}
+
+int main() {
+    // cout << "The sum of 3 and 6 is " << add(3, 6) << endl;
+    // cout << "The sum of 3, 7 and 6 is " << add(3, 7, 6) << endl;
+    cout << "The volume of cuboid / rectangle of 3, 7 and 6 is " << volume(3, 7, 6) << endl;
+    cout << "The volume of cylinder of radius 3 and height 6 is " << volume(3, 6) << endl;
+    cout << "The volume of cube of side 3 is " << volume(3) << endl;
+    
+    return 0;
+}
